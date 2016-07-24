3359c313f0bfbf8786a6ca9cf719a580c51404cd
Gary Luce <lucegary@gmail.com>
2016-07-24 14:57:56 +0100

    Added events.

---
+++ BoutTime/Events.swift
@@ -0,0 +1,36 @@
+//
+//  Events.swift
+//  BoutTime
+//
+//  Created by Gary Luce on 24/07/2016.
+//  Copyright © 2016 gloos. All rights reserved.
+//
+
+import Foundation
+
+let events = ["Martin Luther King Jr.'s 'I have a dream' speech" : 1963,
+              " The Titanic sunk" : 1912,
+              "Filming the MGM Lion" : 1929,
+              "Wilbur Wright flies around the Statue of Liberty" : 1909,
+              "End of World War II" : 1945,
+              "Pearl Harbor bombings" : 1941,
+              "Neil Armstrong walked on the moon" : 1969,
+              "JFK gives 'Ich bin ein Berliner' speech in Berlin" : 1963,
+              "Attacks on New York" : 2001,
+              "The Birth of Christianity" : 0,
+              "Assassination of Archduke Franz Ferdinand of Austria" : 1914,
+              "Invasion of Poland by Germany" : 1939,
+              "Boston Tea Party" : 1773,
+              "Captain Cook Killed" : 1779,
+              "Battle of Jersey" : 1781,
+              "Battle of Trafalgar" : 1805,
+              "London Beer Flood" : 1814,
+              "Death of Napoleon" : 1821,
+              "London University is founded" : 1826,
+              "Dickens publishes a Christmas Carol" : 1843,
+              "iPhone introduced" : 2007,
+              "Margaret Thatcher elected" : 1975,
+              "Apple computers launched" : 1976,
+              "World's first £1 million footballer is bought" : 1979,
+              "First London Marath" : 1981
+            ]
