dba54de69fcc9802c9770d0800294b571cdbb02c
Gary <Gary@ingrids-macbook-pro.local>
2016-07-27 15:42:57 +0100

    Initial commit.

--- BoutTime/Events.swift
+++ BoutTime/Events.swift
@@ -8,29 +8,73 @@
 
 import Foundation
 
-let events = ["Martin Luther King Jr.'s 'I have a dream' speech" : 1963,
-              " The Titanic sunk" : 1912,
-              "Filming the MGM Lion" : 1929,
-              "Wilbur Wright flies around the Statue of Liberty" : 1909,
-              "End of World War II" : 1945,
-              "Pearl Harbor bombings" : 1941,
-              "Neil Armstrong walked on the moon" : 1969,
-              "JFK gives 'Ich bin ein Berliner' speech in Berlin" : 1963,
-              "Attacks on New York" : 2001,
-              "The Birth of Christianity" : 0,
-              "Assassination of Archduke Franz Ferdinand of Austria" : 1914,
-              "Invasion of Poland by Germany" : 1939,
-              "Boston Tea Party" : 1773,
-              "Captain Cook Killed" : 1779,
-              "Battle of Jersey" : 1781,
-              "Battle of Trafalgar" : 1805,
-              "London Beer Flood" : 1814,
-              "Death of Napoleon" : 1821,
-              "London University is founded" : 1826,
-              "Dickens publishes a Christmas Carol" : 1843,
-              "iPhone introduced" : 2007,
-              "Margaret Thatcher elected" : 1975,
-              "Apple computers launched" : 1976,
-              "World's first £1 million footballer is bought" : 1979,
-              "First London Marath" : 1981
-            ]
+class Event {
+    var name: String
+    var year: Int
+    var website: String
+    init(name: String, year: Int, website: String) {
+        self.name = name
+        self.year = year
+        self.website = website
+    }
+}
+
+let lutherKing = Event(name: "Martin Luther King Jr.'s 'I have a dream' speech", year: 1963, website: "https://en.wikipedia.org/wiki/I_Have_a_Dream")
+let titanic = Event(name: "The Titanic sunk", year: 1912, website: "https://en.wikipedia.org/wiki/RMS_Titanic")
+let mgmLion = Event(name: "Filming the MGM Lion", year: 1929, website: "https://en.wikipedia.org/wiki/Leo_the_Lion_(MGM)")
+let wright = Event(name: "Wilbur Wright flies around the Statue of Liberty", year: 1909, website: "https://en.wikipedia.org/wiki/Wright_brothers")
+let ww2 = Event(name: "End of World War II", year: 1945, website: "https://en.wikipedia.org/wiki/World_War_II")
+let pearlHarbor = Event(name: "Pearl Harbor bombings", year: 1941, website: "https://en.wikipedia.org/wiki/Pearl_Harbor")
+let neilArmstrong = Event(name: "Neil Armstrong walked on the moon", year: 1969, website: "https://en.wikipedia.org/wiki/Neil_Armstrong")
+let jfkSpeech = Event(name: "JFK gives 'Ich bin ein Berliner' speech in Berlin", year: 1963, website: "https://storify.com/RTEArchives/president-kennedy-in-ireland")
+let nine11 = Event(name: "Attacks on New York", year: 2001, website: "https://en.wikipedia.org/wiki/September_11_attacks")
+let christianity = Event(name: "The Birth of Christianity", year: 0, website: "google.co.uk")
+let franzFerdinand = Event(name: "Assassination of Archduke Franz Ferdinand of Austria", year: 1914, website: "https://en.wikipedia.org/wiki/Archduke_Franz_Ferdinand_of_Austria")
+let polandInvasion = Event(name: "Invasion of Poland by Germany", year: 1939, website: "https://en.wikipedia.org/wiki/Invasion_of_Poland")
+let bostonTeaParty = Event(name: "Boston Tea Party", year: 1773, website: "https://en.wikipedia.org/wiki/Boston_Tea_Party")
+let cook = Event(name: "Captain Cook Killed", year: 1779, website: "https://en.wikipedia.org/wiki/James_Cook")
+let jersey = Event(name: "Battle of Jersey", year: 1781, website: "https://en.wikipedia.org/wiki/Battle_of_Jersey")
+let trafalgar = Event(name: "Battle of Trafalgar", year: 1805, website: "https://en.wikipedia.org/wiki/Battle_of_Trafalgar")
+let beerFlood = Event(name: "London Beer Flood", year: 1814, website: "https://en.wikipedia.org/wiki/London_Beer_Flood")
+let napoleon = Event(name: "Death of Napoleon" , year: 1821, website: "https://en.wikipedia.org/wiki/Napoleon")
+let londonU = Event(name: "London University is founded", year: 1826, website: "https://en.wikipedia.org/wiki/University_of_London")
+let dickens = Event(name: "Dickens publishes a Christmas Carol", year: 1843, website: "https://en.wikipedia.org/wiki/Charles_Dickens")
+let iPhone = Event(name: "iPhone introduced", year: 2007, website: "https://en.wikipedia.org/wiki/IPhone")
+let thatcher = Event(name: "Margaret Thatcher elected", year: 1975, website: "https://en.wikipedia.org/wiki/Margaret_Thatcher")
+let apple = Event(name: "Apple computers launched", year: 1976, website: "https://en.wikipedia.org/wiki/Apple_Inc.")
+let football = Event(name: "World's first £1 million footballer is bought", year: 1979, website: "https://en.wikipedia.org/wiki/World_football_transfer_record")
+let marathon = Event(name: "First London Marathon", year: 1981, website: "https://en.wikipedia.org/wiki/London_Marathon")
+
+
+let events = [lutherKing,titanic, mgmLion, wright, ww2, pearlHarbor, neilArmstrong, jfkSpeech, nine11, christianity, franzFerdinand, polandInvasion, bostonTeaParty, cook, jersey, trafalgar, beerFlood, napoleon, londonU, dickens, iPhone, thatcher, apple, football, marathon]
+
+//let events = ["Martin Luther King Jr.'s 'I have a dream' speech" : 1963,
+//              " The Titanic sunk" : 1912,
+//              "Filming the MGM Lion" : 1929,
+//              "Wilbur Wright flies around the Statue of Liberty" : 1909,
+//              "End of World War II" : 1945,
+//              "Pearl Harbor bombings" : 1941,
+//              "Neil Armstrong walked on the moon" : 1969,
+//              "JFK gives 'Ich bin ein Berliner' speech in Berlin" : 1963,
+//              "Attacks on New York" : 2001,
+//              "The Birth of Christianity" : 0,
+//              "Assassination of Archduke Franz Ferdinand of Austria" : 1914,
+//              "Invasion of Poland by Germany" : 1939,
+//              "Boston Tea Party" : 1773,
+//              "Captain Cook Killed" : 1779,
+//              "Battle of Jersey" : 1781,
+//              "Battle of Trafalgar" : 1805,
+//              "London Beer Flood" : 1814,
+//              "Death of Napoleon" : 1821,
+//              "London University is founded" : 1826,
+//              "Dickens publishes a Christmas Carol" : 1843,
+//              "iPhone introduced" : 2007,
+//              "Margaret Thatcher elected" : 1975,
+//              "Apple computers launched" : 1976,
+//              "World's first £1 million footballer is bought" : 1979,
+//              "First London Marath" : 1981
+//            ]
+
+enum Vote {
+    case upvoted, downvoted
+}
--- BoutTime/ViewController.swift
+++ BoutTime/ViewController.swift
@@ -7,20 +7,154 @@
 //
 
 import UIKit
+import GameKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet var textViews: [UITextView]!
+    @IBAction func topButtonDownvote(sender: AnyObject) {
+        swap(&textViews[0].text, &textViews[1].text)
+        swap(&textViews[0].tag, &textViews[1].tag)
+  
+    }
+    @IBAction func middleButtonDownvote(sender: AnyObject) {
+        swap(&textViews[1].text, &textViews[2].text)
+        swap(&textViews[1].tag, &textViews[2].tag)
+
+    }
+    @IBAction func bottomButtonDownvote(sender: AnyObject) {
+        swap(&textViews[2].text, &textViews[3].text)
+        swap(&textViews[2].tag, &textViews[3].tag)
+
+    }
+    @IBAction func topButtonUpvote(sender: AnyObject) {
+        swap(&textViews[1].text, &textViews[0].text)
+        swap(&textViews[1].tag, &textViews[0].tag)
+
+    }
+    @IBAction func middleButtonUpvote(sender: AnyObject) {
+        swap(&textViews[2].text, &textViews[1].text)
+        swap(&textViews[2].tag, &textViews[1].tag)
+
+    }
+    @IBAction func lastButtonUpvote(sender: AnyObject) {
+        swap(&textViews[3].text, &textViews[2].text)
+        swap(&textViews[3].tag, &textViews[2].tag)
+
+    }
+    @IBAction func nextRoundButton(sender: AnyObject) {
+        if round >= 6 {
+            //add view controller to display the score
+        } else {
+            launchNextRound()
+        }
+        
+    }
+    @IBOutlet weak var nextRound: UIButton!
+    
+    var round: Int = 0
+    var blacklist = [Int]()
+    var yearsQuizRound = [Int]()
+    var selectedYears = [Int]()
+    var timer = NSTimer()
+    var counter = 60
+    var isGameOver = false
+    var totalScore = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print(events.count)
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        print(textViews.count)
+        displayQuestions()
+        startTimer()
     }
 
 
+
+    //MARK: Populate text fields
+    
+    func displayQuestions() {
+            for textView in textViews {
+                let questionIndex = randomNumber()
+                if blacklist.contains(questionIndex) {
+                    reset()
+                    displayQuestions()
+                }
+                blacklist.append(questionIndex)
+                yearsQuizRound.append(events[questionIndex].year)
+                textView.text = events[questionIndex].name
+                textView.tag = events[questionIndex].year
+                print("Blacklist: \(blacklist)")
+            }
+        yearsQuizRound.sortInPlace({ $0 < $1 })
+           }
+    
+    func checkAnswers() {
+        selectedYears.removeAll()
+        for textView in textViews {
+            selectedYears.append(textView.tag)
+        }
+        if selectedYears == yearsQuizRound {
+            stopTimer()
+            if let image = UIImage(named: "next_round_success") {
+                nextRound.setImage(image, forState: .Normal)
+            }
+            totalScore += 1
+            round += 1
+            
+        } else {
+            print("Keep going!")
+            print(selectedYears)
+            print(yearsQuizRound)
+        }
+    }
+    
+    //MARK: Helper methods
+    
+    func reset() {
+        for textView in textViews {
+            textView.text = nil
+        }
+        blacklist.removeAll()
+        yearsQuizRound.removeAll()
+    }
+    
+    func launchNextRound() {
+        nextRound.setImage(nil, forState: .Normal)
+        reset()
+        displayQuestions()
+        startTimer()
+    }
+    
+    
+    func randomNumber() -> Int {
+        return GKRandomSource.sharedRandom().nextIntWithUpperBound(events.count)
+    }
+
+    //MARK: Timer methods
+    
+    func startTimer() {
+        self.counter = 60
+        nextRound.setTitle(String(self.counter), forState: .Normal)
+        self.timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+    }
+    
+    func updateCounter() {
+        if self.counter > 0 && isGameOver == false {
+            nextRound.setTitle(String(self.counter--), forState: .Normal)
+        } else if self.counter == 0 {
+             nextRound.setTitle(String(0), forState: .Normal)
+            stopTimer()
+            if let image = UIImage(named: "next_round_fail") {
+                nextRound.setImage(image, forState: .Normal)
+            }
+            round += 1
+            
+        }
+    }
+    
+    func stopTimer() {
+         self.timer.invalidate()
+    }
 }
 
